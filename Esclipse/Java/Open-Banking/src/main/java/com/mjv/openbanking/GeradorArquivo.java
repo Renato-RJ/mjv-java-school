62607670b8b151f89b8821697fa23aacf9a5ac73
Renato-RJ <renato_santosgs@hotmail.com>
2022-04-22 14:58:27 -0300

    Correção variável e nome txt

--- Esclipse/Java/Open-Banking/src/main/java/com/mjv/openbanking/GeradorArquivo.java
+++ Esclipse/Java/Open-Banking/src/main/java/com/mjv/openbanking/GeradorArquivo.java
@@ -8,15 +8,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class GeradorArquivo {
-	public void escreverDisco (String ConteudoGerado, Movimentacao movimentacao){
+	public void escreverDisco (String conteudoGerado, Movimentacao movimentacao){
 		String dataOperacao = movimentacao.getDataMovimentacao().toString();
 		
 		try {
 			File diretorio = new File("c:\\estudos\\banco_central\\movimentacoes\\");
 			if (! diretorio.exists())
 				diretorio.mkdirs();
-			Path enderecoDiretorio = Paths.get(diretorio.getAbsolutePath() +"\\"+ dataOperacao + ".txt");
-			Files.write(enderecoDiretorio, ConteudoGerado.getBytes(StandardCharsets.UTF_8));
+			Path enderecoDiretorio = Paths.get(diretorio.getAbsolutePath() +"\\"+ dataOperacao + "_movimentacao.txt");
+			Files.write(enderecoDiretorio, conteudoGerado.getBytes(StandardCharsets.UTF_8));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
